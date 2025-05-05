1418723bb9975c8e977190cd9792f40ecc828c6a
BurgerMike <mikelizondojr@hotmail.com>
2025-05-04 20:29:09 -0600

    Initial Commit

---
+++ Package.swift
@@ -0,0 +1,24 @@
+// swift-tools-version: 6.1
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "SwiftSocket.ioNative",
+    products: [
+        // Products define the executables and libraries a package produces, making them visible to other packages.
+        .library(
+            name: "SwiftSocket.ioNative",
+            targets: ["SwiftSocket.ioNative"]),
+    ],
+    targets: [
+        // Targets are the basic building blocks of a package, defining a module or a test suite.
+        // Targets can depend on other targets in this package and products from dependencies.
+        .target(
+            name: "SwiftSocket.ioNative"),
+        .testTarget(
+            name: "SwiftSocket.ioNativeTests",
+            dependencies: ["SwiftSocket.ioNative"]
+        ),
+    ]
+)
