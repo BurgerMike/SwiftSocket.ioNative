56d4ace0efa97768055f54f4624b6e7479dafec1
BurgerMike <mikelizondojr@hotmail.com>
2025-05-13 12:14:23 -0600

    P
    s

--- Sources/SwiftSocket.ioNative/SocketEvent.swift
+++ Sources/SwiftSocket.ioNative/SocketEvent.swift
@@ -10,13 +10,11 @@ public struct SocketEvent: Codable {
     public let event: String
     public let data: Data?
 
-    // ✅ Corrección: Solo requiere Encodable para emitir
     public init<T: Encodable>(event: String, data: T?) {
         self.event = event
         self.data = try? JSONEncoder().encode(data)
     }
 
-    // ✅ Utilidad para decodificar la data si se espera respuesta del servidor
     public func decodedData<T: Decodable>(as type: T.Type) -> T? {
         guard let data = data else { return nil }
         return try? JSONDecoder().decode(T.self, from: data)
